dfa783e5a522afcc610ec93beb685c7c532876b1
张欣 <106731421+zhangxin158@users.noreply.github.com>
2022-10-21 10:37:38 +0800

    Update main.c
    test

--- Src/main.c
+++ Src/main.c
@@ -74,7 +74,7 @@ static void MX_USART1_UART_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+//000
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
