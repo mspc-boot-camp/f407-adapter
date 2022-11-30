d69eb96b2d8617b03a987a688aceb01a06099638
Hugo Chiang <1483569698@qq.com>
2022-11-30 09:34:40 +0800

    fix spi-cdc tx

--- Src/main.c
+++ Src/main.c
@@ -27,10 +27,6 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "usbd_cdc_if.h"
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -61,187 +57,170 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-uint8_t SPI_TXbuf[59]={0xEB,0x90,0xC0,0x0B,0x0A,0x0C,0x0A,0xAB,0xBD,0xBC,
-                       0xDC,0xAC,0xDF,0x0A,0x0A,0xCD,0x0A,0xA0,0xCA,0x0D,
-                       0xDC,0xAC,0xDF,0x0A,0x0A,0xCD,0x0A,0x0A,0xCA,0x0C,
-                       0xAC,0xAC,0xDF,0x0A,0x0A,0xCD,0x0A,0xA0,0xCA,0xB0,
-                       0xDB,0xAC,0xDF,0x0B,0x0A,0xCD,0x0A,0x0A,0xCA,0xC0,
-                       0x0C,0xAC,0xDF,0x0A,0x0B,0xCD,0x0A,0x0D,0x0A};
+uint8_t SPI_TXbuf[59] = {0xEB, 0x90, 0xC0, 0x0B, 0x0A, 0x0C, 0x0A, 0xAB, 0xBD, 0xBC,
+                         0xDC, 0xAC, 0xDF, 0x0A, 0x0A, 0xCD, 0x0A, 0xA0, 0xCA, 0x0D,
+                         0xDC, 0xAC, 0xDF, 0x0A, 0x0A, 0xCD, 0x0A, 0x0A, 0xCA, 0x0C,
+                         0xAC, 0xAC, 0xDF, 0x0A, 0x0A, 0xCD, 0x0A, 0xA0, 0xCA, 0xB0,
+                         0xDB, 0xAC, 0xDF, 0x0B, 0x0A, 0xCD, 0x0A, 0x0A, 0xCA, 0xC0,
+                         0x0C, 0xAC, 0xDF, 0x0A, 0x0B, 0xCD, 0x0A, 0x0D, 0x0A};
 uint8_t SPI_RXbuf[59];
 
-int tim_flag=0;
-int spi_flag=0;
+uint8_t blankBuf[59] = {0};
+uint8_t usbTxBuf[59] = {0};
+
+int tim_flag = 0;
+int spi_flag = 0;
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+    /* USER CODE BEGIN 1 */
     // 000
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* USER CODE BEGIN Init */
-  /* USER CODE END Init */
+    /* USER CODE BEGIN Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_SPI1_Init();
-  MX_USB_DEVICE_Init();
-  MX_TIM2_Init();
-  /* USER CODE BEGIN 2 */
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_DMA_Init();
+    MX_SPI1_Init();
+    MX_USB_DEVICE_Init();
+    MX_TIM2_Init();
+    /* USER CODE BEGIN 2 */
 
-    HAL_SPI_TransmitReceive_DMA(&hspi1,SPI_TXbuf, SPI_RXbuf,58);  
+    HAL_SPI_TransmitReceive_DMA(&hspi1, SPI_TXbuf, SPI_RXbuf, 59);
     CDC_Transmit_FS(SPI_RXbuf, sizeof(SPI_RXbuf));
     HAL_TIM_Base_Start_IT(&htim2);
-  /* USER CODE END 2 */
+    /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
     while (1)
     {
-      
-    
-    /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
-     if(tim_flag==1)
-  {
-      tim_flag=0;
-      HAL_SPI_TransmitReceive_DMA(&hspi1,SPI_TXbuf, SPI_RXbuf,58); 
-      
-  }
-    if(spi_flag==1)
-  {
-      spi_flag=0;
-      CDC_Transmit_FS(SPI_RXbuf, sizeof(SPI_RXbuf));  
-  }
-     
+        /* USER CODE END WHILE */
+
+        /* USER CODE BEGIN 3 */
+        if (tim_flag == 1)
+        {
+            tim_flag = 0;
+            HAL_SPI_TransmitReceive_DMA(&hspi1, SPI_TXbuf, SPI_RXbuf, 59);
+        }
+        if ( memcmp(SPI_RXbuf, blankBuf, 59) != 0 )
+        {
+            CDC_Transmit_FS(SPI_RXbuf, sizeof(SPI_RXbuf));
+            memset(SPI_RXbuf, 0, sizeof(SPI_RXbuf));
+        }
     }
-  /* USER CODE END 3 */
+    /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    /** Configure the main internal regulator output voltage
+     */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 4;
-  RCC_OscInitStruct.PLL.PLLN = 168;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 7;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 4;
+    RCC_OscInitStruct.PLL.PLLN = 168;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 7;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+    {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
 
-
-//SPI demo
-   void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi)
-{
-    //UNUSED(hspi1);
-    spi_flag=1;
-   // HAL_SPI_TransmitReceive_DMA(&hspi1,SPI_TXbuf, SPI_RXbuf,58);
-    
-}   
-
-<<<<<<< Updated upstream
-//timer 2ms
-=======
-/* USER CODE BEGIN 4 */
 void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi)
 {
     // UNUSED(hspi1);
-    spi_flag = 1;
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
-    CDC_Transmit_FS(SPI_RXbuf, sizeof(SPI_RXbuf));
+    memcpy(SPI_RXbuf, usbTxBuf, sizeof(SPI_RXbuf));
+    // CDC_Transmit_FS(SPI_RXbuf, sizeof(SPI_RXbuf));
 }
->>>>>>> Stashed changes
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-  if (htim == (&htim2))
-  
-     tim_flag=1;
-     
+    if (htim == (&htim2))
+
+        tim_flag = 1;
 }
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
+    /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1)
     {
     }
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
+    /* USER CODE BEGIN 6 */
     /* User can add his own implementation to report the file name and line number,
        ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
